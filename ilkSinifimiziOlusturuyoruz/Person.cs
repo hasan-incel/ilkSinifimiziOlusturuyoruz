b2e115d9f6f4e9c2ddff5e80674deac43642e17e
Hasan İncel <h_incel@hotmail.com>
2024-08-06 17:27:39 +0300

    Add project files.

---
+++ ilkSinifimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilkSinifimiziOlusturuyoruz
+{
+    public class Person
+    {
+        public string ad;
+        public string soyad;
+        public string statue;
+        public int dogumTarihi;
+
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+            set
+            {
+                ad = value;
+            }
+        }
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+            set
+            {
+                soyad = value;
+            }
+        }
+        public string Statue
+        {
+            get
+            {
+                return statue;
+            }
+            set
+            {
+                statue = value;
+            }
+        }
+        public int DogumTarihi
+        {
+            get
+            {
+                return dogumTarihi;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    dogumTarihi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış doğum tarihi girdiniz");
+                }
+            }
+        }
+        public void Bilgiler()
+        {
+            Console.WriteLine("\nAdı: " + ad);
+            Console.WriteLine("Soyadı: " + soyad);
+            Console.WriteLine("Statü: " + statue);
+            Console.WriteLine("Doğum Tarihi: " + dogumTarihi);
+        }
+    }
+
+}
---
+++ ilkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,37 @@
+﻿using ilkSinifimiziOlusturuyoruz;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person();
+
+        person.ad = "Mert";
+        person.soyad = "Sert";
+        person.statue = "Öğrenci";
+        person.dogumTarihi = 1998;
+        person.Bilgiler();
+
+
+        person.ad = "Derin";
+        person.soyad = "Merin";
+        person.statue = "Öğrenci";
+        person.dogumTarihi = 1996;
+        person.Bilgiler();
+
+
+        person.ad = "Tan";
+        person.soyad = "Büyük";
+        person.statue = "Öğretmen";
+        person.dogumTarihi = 1975;
+        person.Bilgiler();
+
+
+        person.ad = "Sıla";
+        person.soyad = "Küçük";
+        person.statue = "Öğretmen";
+        person.dogumTarihi = 1980;
+        person.Bilgiler();
+
+    }
+}
